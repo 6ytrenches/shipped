d8bc3d78911cc8c1aed98bec0f5e87545cfb3869
miguel myers <myers.m007@gmail.com>
2017-03-01 14:30:10 -0500

    ..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
   	if current_user.shipper == "Shipper"
   		boats_path
-  	else 
+  	else
   		jobs_path
   	end
   end
